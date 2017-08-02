030ca1e8c97c9847723e3126506c918ce190532c
张超 <zc@zhangchaodeMacBook-Pro.local>
2017-08-02 15:43:38 +0800

    界面管理器

---
+++ app/src/main/java/com/robin8/rb/util/ActivityManagerUtils.java
@@ -0,0 +1,94 @@
+package com.robin8.rb.util;
+
+import android.app.Activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ Created by zc on 2017/7/31. */
+
+public class ActivityManagerUtils {
+    private static ActivityManagerUtils mActivityManagerUtils;
+
+
+    static {
+        mActivityManagerUtils = new ActivityManagerUtils();
+    }
+
+    private ActivityManagerUtils() {
+        /**
+         * 这里面写一些需要执行初始化的工作
+         */
+    }
+
+    public static ActivityManagerUtils getInstance() {
+        return mActivityManagerUtils;
+
+    }
+
+    /**
+     打开的activity
+     **/
+
+    private List<Activity> activities = new ArrayList<Activity>();
+
+
+    /**
+     新建了一个activity
+     @param activity
+     */
+
+    public void addActivity(Activity activity) {
+        activities.add(activity);
+    }
+
+    /**
+     结束指定的Activity
+     @param activity
+     */
+
+    public void finishActivity(Activity activity) {
+
+        if (activity != null) {
+            this.activities.remove(activity);
+            activity.finish();
+        }
+    }
+
+    /**
+     应用退出，结束所有的activity
+     */
+
+    public void exit() {
+        try {
+            for (Activity activity : activities) {
+                if (activity != null) {
+                    activity.finish();
+                }
+            }
+            System.exit(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    /**
+     结束指定类名的Activity
+     */
+    public void finishActivityclass(Class<?> cls) {
+        if (activities != null) {
+            for (Activity activity : activities) {
+                if (activity.getClass().equals(cls)) {
+                    this.activities.remove(activity);
+                    finishActivity(activity);
+                    break;
+                }
+            }
+        }
+
+    }
+
+}
