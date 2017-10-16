f10028cc57fc40c2fc7cc34d65a0a1d67ef4d659
张超 <zc@zhangchaodeMacBook-Pro.local>
2017-10-16 10:47:06 +0800

    Android使用my_show接口对一元夺宝进行隐藏

--- app/src/main/java/com/robin8/rb/module/mine/model/MineShowModel.java
+++ app/src/main/java/com/robin8/rb/module/mine/model/MineShowModel.java
@@ -1,29 +1,57 @@
 package com.robin8.rb.module.mine.model;
 
-import com.robin8.rb.model.BaseBean;
-
+import java.io.Serializable;
 import java.util.List;
 
 /**
  * Created by IBM on 2016/8/4.
  */
-public class MineShowModel extends BaseBean {
+public class MineShowModel implements Serializable {
 
     /**
-     * id : 55712
-     * name : 15121197517
-     * kol_role : public
-     * role_apply_status : pending
-     * role_check_remark : null
-     * max_campaign_click : 15
-     * max_campaign_earn_money : 8.0
-     * campaign_total_income : 29.65
-     * avg_campaign_credit : 3.294444444444444444444444444
-     * avatar_url : http://7xozqe.com2.z0.glb.qiniucdn.com/uploads/kol/avatar/55712/4ec872652f.jpeg
-     * tags : [{"name":"food","label":"美食"},{"name":"babies","label":"母婴"},{"name":"travel","label":"旅游"},{"name":"hotel","label":"酒店"},{"name":"education","label":"教育"},{"name":"fitness","label":"健身"},{"name":"finance","label":"财经"},{"name":"beauty","label":"美妆"},{"name":"auto","label":"汽车"},{"name":"airline","label":"航空"},{"name":"ce","label":"消费电子"},{"name":"camera","label":"摄影"},{"name":"mobile","label":"手机"}]
+     * error : 0
+     * hide : 1
+     * detail : 9
+     * kol : {"id":109050,"name":"159****9098","kol_role":"public","role_apply_status":"pending","role_check_remark":null,"max_campaign_click":null,"max_campaign_earn_money":0,"campaign_total_income":"0.0","avg_campaign_credit":null,"avatar_url":"http://7xozqe.com2.z0.glb.qiniucdn.com/uploads/kol/avatar/109050/c1a7dcb670.jpg!avatar","tags":[{"name":"airline","label":"航空"},{"name":"ce","label":"消费电子"},{"name":"appliances","label":"家电"}]}
+     * is_open_indiana : true
      */
 
+    private int error;
+    private int hide;
+    private int detail;
     private KolBean kol;
+    private boolean is_open_indiana;
+
+    public int getError() {
+        return error;
+    }
+
+    public void setError(int error) {
+        this.error = error;
+    }
+
+    public int getHide() {
+        return hide;
+    }
+
+    public void setHide(int hide) {
+        this.hide = hide;
+    }
+
+    public int getDetail() {
+        return detail;
+    }
+
+    public void setDetail(int detail) {
+        this.detail = detail;
+    }
+    public boolean isIs_open_indiana() {
+        return is_open_indiana;
+    }
+
+    public void setIs_open_indiana(boolean is_open_indiana) {
+        this.is_open_indiana = is_open_indiana;
+    }
 
     public KolBean getKol() {
         return kol;
@@ -33,7 +61,7 @@ public class MineShowModel extends BaseBean {
         this.kol = kol;
     }
 
-    public static class KolBean {
+    public static class KolBean implements Serializable{
         private int id;
         private String name;
         private String kol_role;
