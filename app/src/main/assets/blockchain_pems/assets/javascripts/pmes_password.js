4193daac508bbe9e8ca2d9c48c6094155ca7a640
xingchong <cxing@robin8.com>
2018-09-18 16:38:29 +0800

    fix webview

--- app/src/main/assets/blockchain_pems/assets/javascripts/pmes_password.js
+++ app/src/main/assets/blockchain_pems/assets/javascripts/pmes_password.js
@@ -2,13 +2,6 @@ document.addEventListener('touchstart', function() {}, true);
 
 $(document).ready(function() {
   var get_put_address = '';
-  if (typeof jwPut != 'undefined') {
-    var native_put_address = jwPut.put_put_address();
-    if (native_put_address != '') {
-      get_put_address = native_put_address;
-    }
-  }
-
   var loading = new CreateLoader();
   // 重置密码
   $('#pmes_password_submit_btn').click(function(event) {
@@ -36,6 +29,13 @@ $(document).ready(function() {
     if (mnemonic == '') {
       createAlert('请输入您的安全码');
       return false;
+    }
+
+    if (typeof jwPut != 'undefined') {
+      var native_put_address = jwPut.put_put_address();
+      if (native_put_address != '') {
+        get_put_address = native_put_address;
+      }
     }
     loading.show();
 
